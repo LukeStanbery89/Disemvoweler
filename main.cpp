a69135e3c0e28d88c2f28c7813d2b6194c151dc6
Luke Stanbery <lukestanbery89@gmail.com>
2014-10-31 00:17:19 -0500

    First commit.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Declarations
+    string input;
+
+    // Get input
+    cout << "Please enter a phrase: ";
+    cin >> input;
+
+
+
+    return 0;
+}
